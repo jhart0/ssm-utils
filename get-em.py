5cee69438ea58495f2b102eab0487d23e4d5877b
Josh Hart <josh.hart@eposnow.com>
2020-08-26 15:38:39 +0100

    take in prefix to get

--- get-em.py
+++ get-em.py
@@ -2,13 +2,13 @@ import csv
 import sys
 import boto3
 
-def load(file_path):
+def load(file_path, prefix):
     session = boto3.Session(profile_name=profile)
     SSM = session.client('ssm')
     paginator = SSM.get_paginator('get_parameters_by_path')
     with open(file_path, 'w+', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        params_iterator = paginator.paginate(Path='/', Recursive=True)
+        params_iterator = paginator.paginate(Path=prefix, Recursive=True)
         for page in params_iterator:
             for item in page['Parameters']:
                 writer.writerow([item['Name'], item['Value']])
@@ -16,4 +16,5 @@ def load(file_path):
 if __name__ == '__main__':
     file_path = sys.argv[1]
     profile = sys.argv[2]
-    load(file_path)
+    prefix = sys.argv[3]
+    load(file_path, prefix)
